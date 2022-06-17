8a30cc52dee48374090f316daff340ea1ceca971
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-17 14:44:13 +0300

    Add store and message

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import messageReducer from './message';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const store = configureStore({
+	reducer: {
+		message: messageReducer,
+	},
 });
+export default store;
